a8b1fcdf7b29eca44cc5e5c79d311976797a6554
powrhouseofthecell <theshahzuhaib96@gmail.com>
2024-02-10 21:15:46 +0530

    chore: add events router

--- mentor-be/src/routes/router.ts
+++ mentor-be/src/routes/router.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 const router = express.Router();
+
 import auth_router from './auth';
+import events_router from './event';
 
 router.use('/auth', auth_router);
-
+router.use('/events', events_router);
 export default router;
