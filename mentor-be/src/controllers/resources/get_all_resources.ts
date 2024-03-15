f834e90ad86ca4be8ec529ef9efaf438669ed649
powrhouseofthecell <theshahzuhaib96@gmail.com>
2024-03-15 20:40:00 +0530

    chore: get all resources

--- mentor-be/src/controllers/resources/get_all_resources.ts
+++ mentor-be/src/controllers/resources/get_all_resources.ts
@@ -1,8 +1,28 @@
-import { Request, Response, NextFunction } from "express";
-export default function get_all_resources(
-  _req: Request,
-  res: Response,
-  _next: NextFunction,
-) {
-  res.send("getting all resources");
+import { Request, Response, NextFunction } from 'express';
+import path from 'path';
+import fs from 'fs';
+
+export default function get_all_resources(_req: Request, res: Response, _next: NextFunction) {
+  const path_down = path.join(__dirname, '../../public/uploads');
+  console.log(path_down);
+
+  fs.readdir(path_down, (err, files) => {
+    if (err) {
+      console.error('Error reading directory:', err);
+      return;
+    }
+
+    // Filter out directories (if any)
+    files = files.filter((file) => fs.statSync(path.join(path_down, file)).isFile());
+
+    // Get file names and types
+    const file_details = files.map((file) => {
+      return {
+        name: file,
+        type: path.extname(file).slice(1),
+      };
+    });
+
+    res.json(file_details);
+  });
 }
