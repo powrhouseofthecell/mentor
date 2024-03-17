21feddb93fa836b2724a3cd1e274d86c6156ffaf
powrhouseofthecell <theshahzuhaib96@gmail.com>
2024-03-17 19:24:24 +0530

    chore: role check error message

--- mentor-be/src/middleware/role_check.ts
+++ mentor-be/src/middleware/role_check.ts
@@ -1,17 +1,13 @@
-import { Response, NextFunction } from "express";
+import { Response, NextFunction } from 'express';
 
-import { Custom_Req } from "../types/custom_params";
+import { Custom_Req } from '../types/custom_params';
 
-export default function role_check(
-  req: Custom_Req,
-  _res: Response,
-  next: NextFunction,
-) {
+export default function role_check(req: Custom_Req, _res: Response, next: NextFunction) {
   try {
-    if (req.user.role === "admin" || req.user.role === "mentor") {
+    if (req.user.role === 'admin' || req.user.role === 'mentor') {
       next();
     } else {
-      throw new Error("only admin/mentor can create events.");
+      throw new Error('only admins or mentors can perform this action.');
     }
   } catch (err) {
     next(err);
