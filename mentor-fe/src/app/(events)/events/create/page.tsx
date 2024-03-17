81e87d6088c02483dce6a3087f3bfcb6ae173079
powrhouseofthecell <theshahzuhaib96@gmail.com>
2024-03-17 23:09:52 +0530

    feat: create event

---
+++ mentor-fe/src/app/(events)/events/create/page.tsx
@@ -0,0 +1,114 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { Button } from '@/components/ui/button';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+import Date_Picker_With_Range from '@/components/date-picker-range';
+import { BASE_URL } from '../../../../../config';
+import { useToast } from '@/components/ui/use-toast';
+import axios from 'axios';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+const formSchema = z.object({
+  event_name: z.any(),
+  event_description: z.any(),
+  event_date: z.any(),
+});
+
+export default function Create_Event({ id }: any) {
+  const router = useRouter();
+  const [date, set_date] = useState<any>('');
+  const { toast } = useToast();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  function get_date(date: any) {
+    set_date(date);
+  }
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const url = `${BASE_URL}/events/create`;
+
+    const { event_description, event_name } = values;
+    // const event_date = date;
+    const event_date = '12-08-2024';
+
+    try {
+      const response = await axios({
+        method: 'POST',
+        url,
+        data: { event_description, event_name, event_date },
+        withCredentials: true,
+      });
+      if (!response.data.error) {
+        toast({
+          description: 'event created successfully',
+        });
+      }
+      router.push('/events');
+    } catch (error: any) {
+      toast({
+        variant: 'destructive',
+        title: 'Uh oh! Something went wrong.',
+        description: `${error.response.data.message}`,
+      });
+    }
+    // location.reload();
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8 pt-8 w-3/6 mx-auto'>
+        <FormField
+          control={form.control}
+          name='event_name'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Event name</FormLabel>
+              <FormControl>
+                <Input placeholder='' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {/*  */}
+        <FormField
+          control={form.control}
+          name='event_description'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Event name</FormLabel>
+              <FormControl>
+                <Textarea placeholder='Type your description here' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {/*  */}
+        <FormField
+          control={form.control}
+          name='event_date'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Event date</FormLabel>
+              <FormControl>
+                <Date_Picker_With_Range get_date={get_date} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type='submit'>Submit</Button>
+      </form>
+    </Form>
+  );
+}
