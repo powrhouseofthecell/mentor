68a77c8821dea503ba8633309477f257abdc2759
powrhouseofthecell <theshahzuhaib96@gmail.com>
2024-03-19 20:17:28 +0530

    refactor: remove comments

--- mentor-be/src/controllers/resources/get_all_resources.ts
+++ mentor-be/src/controllers/resources/get_all_resources.ts
@@ -1,18 +1,23 @@
-import { Request, Response, NextFunction } from 'express';
-import path from 'path';
-import fs from 'fs';
+import { Request, Response, NextFunction } from "express";
+import path from "path";
+import fs from "fs";
 
-export default function get_all_resources(_req: Request, res: Response, _next: NextFunction) {
-  const path_down = path.join(__dirname, '../../public/uploads');
+export default function get_all_resources(
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) {
+  const path_down = path.join(__dirname, "../../public/uploads");
 
   fs.readdir(path_down, (err, files) => {
     if (err) {
-      console.error('Error reading directory:', err);
       return;
     }
 
     // Filter out directories (if any)
-    files = files.filter((file) => fs.statSync(path.join(path_down, file)).isFile());
+    files = files.filter((file) =>
+      fs.statSync(path.join(path_down, file)).isFile(),
+    );
 
     // Get file names and types
     const file_details = files.map((file) => {
--- mentor-be/src/controllers/resources/upload.ts
+++ mentor-be/src/controllers/resources/upload.ts
@@ -1,7 +1,11 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from "express";
 
-export default function upload(_req: Request, res: Response, _next: NextFunction) {
+export default function upload(
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) {
   res.status(200).json({
-    message: 'file uploaded successfully',
+    message: "file uploaded successfully",
   });
 }
