5bc2021339e0c1f25c643a1f21a765df65ecda62
powrhouseofthecell <theshahzuhaib96@gmail.com>
2024-03-23 13:55:47 +0530

    fix: cors localhost ip

--- mentor-be/src/app.ts
+++ mentor-be/src/app.ts
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
     credentials: true,
   }),
 );
