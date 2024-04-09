07b009faa10a64f48deea7c674be6b542d3563b9
powrhouseofthecell <theshahzuhaib96@gmail.com>
2024-04-09 10:57:21 +0530

    fmt: nvim

--- mentor-be/src/server.ts
+++ mentor-be/src/server.ts
@@ -1,19 +1,22 @@
-import path from 'path';
-import fs from 'fs';
-import https from 'https';
-import http from 'http';
+import path from "path";
+import fs from "fs";
+import https from "https";
+import http from "http";
 
-import app from './app';
-import create_db_con from './utils/db/create_con';
+import app from "./app";
+import create_db_con from "./utils/db/create_con";
 
-const DB_URI = process.env.DB_URI?.replace('<password>', process.env.DB_PASSWORD as string) as string;
+const DB_URI = process.env.DB_URI?.replace(
+  "<password>",
+  process.env.DB_PASSWORD as string,
+) as string;
 
 const HTTP_PORT = process.env.HTTP_PORT;
 const HTTPS_PORT = process.env.HTTPS_PORT;
-
 const SSL_CERT_PATH = process.env.SSL_CERT_PATH!;
-const key_file_path = path.join(SSL_CERT_PATH, 'key.pem');
-const cert_file_path = path.join(SSL_CERT_PATH, 'cert.pem');
+
+const key_file_path = path.join(SSL_CERT_PATH, "key.pem");
+const cert_file_path = path.join(SSL_CERT_PATH, "cert.pem");
 
 const http_server = http.createServer(app);
 
@@ -22,12 +25,12 @@ const https_server = https.createServer(
     key: fs.readFileSync(key_file_path),
     cert: fs.readFileSync(cert_file_path),
   },
-  app
+  app,
 );
 
 create_db_con(DB_URI)
   .then(() => {
-    console.log('✔︎ DB Connected');
+    console.log("✔︎ DB Connected");
     http_server.listen(HTTP_PORT, () => {
       console.log(`🚀 http server started on port ${HTTP_PORT}`);
     });
@@ -36,5 +39,5 @@ create_db_con(DB_URI)
     });
   })
   .catch((err) => {
-    console.log('DB not connected', err);
+    console.log("DB not connected", err);
   });
