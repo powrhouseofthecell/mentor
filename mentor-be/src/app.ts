9eb1576ff80dfd51af8918bfa9f6b48edc58265d
powrhouseofthecell <theshahzuhaib96@gmail.com>
2024-05-01 18:57:53 +0530

    chore: handle dup email

--- mentor-be/src/app.ts
+++ mentor-be/src/app.ts
@@ -27,6 +27,11 @@ app.use("/api/v1", router);
 app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
   // handle the status code properly
   // 1. Have it based on the err.message from a json file?
+  if (err.message.includes("E11000 duplicate key error collection")) {
+    res
+      .status(400)
+      .send({ error: true, message: "Email is already registered" });
+  }
   res.status(400).send({ error: true, message: err.message });
 });
 
