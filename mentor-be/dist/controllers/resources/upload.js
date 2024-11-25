65094d91aaaf8f556719a0b57673a84baf50738f
powrhouseofthecell <theshahzuhaib96@gmail.com>
2024-11-25 16:15:57 +0530

    fix: ignore dist

--- mentor-be/dist/controllers/resources/upload.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = upload;
-function upload(_req, res, _next) {
-    res.status(200).json({
-        message: "file uploaded successfully",
-    });
-}
-//# sourceMappingURL=upload.js.map
